6a4ba50ca1b0740037e0e7d69bef31534bb2363c
Noam Liza <noamka@matrix.co.il>
2024-06-14 17:52:07 +0300

    Initial commit

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 // src/App.js
 import React from "react";
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import { BrowserRouter as Router, Route, Routes, Navigate  } from "react-router-dom";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Home from "./pages/Home";
@@ -15,8 +15,8 @@ const App = () => {
       <GlobalStyles />
       <Header />
       <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/Portfolio/" element={<Home />} />
+        <Route path="/" element={<Navigate to="/home" />} />
+        <Route path="/home" element={<Home />} />
         <Route path="/about" element={<About />} />
         <Route path="/projects" element={<Projects />} />
         <Route path="/contact" element={<Contact />} />
