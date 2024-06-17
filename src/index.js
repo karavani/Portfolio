847714566f7611294b536fc7f0fdd9be0aafd894
Noam Liza <noamka@matrix.co.il>
2024-06-17 18:53:38 +0300

    Ensure correct index.html and React components

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import ReactDOM from 'react-dom';
 import { HashRouter } from 'react-router-dom';
 import App from './App';
 import './index.css';
+import GlobalStyles from "./styles/GlobalStyles";
 
 ReactDOM.render(
   <React.StrictMode>
+    <GlobalStyles />
     <HashRouter>
       <App />
     </HashRouter>
